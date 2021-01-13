fe2f1926b15854526a2fa68a51e40ab18b5099ed
MaxwellDAnderson <maxwell.d.anderson@gmail.com>
2021-01-13 15:37:53 -0600

    removed unnecessary import statements

--- 8-character-password-generator.py
+++ 8-character-password-generator.py
@@ -1,11 +1,7 @@
-import hashlib
 import string
 import secrets
-import cryptography
-from cryptography.fernet import Fernet
 
-
-# Generate an eight-character alphanumeric password:
+# Generate an eight-character password:
 alphabet8 = string.ascii_letters + string.digits
 password8 = "".join(secrets.choice(alphabet8) for i in range(8))
 print(password8)
