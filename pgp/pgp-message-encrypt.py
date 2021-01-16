22ca74df2831cc32a021915984cd5a44918bd662
MaxwellDAnderson <maxwell.d.anderson@gmail.com>
2021-01-16 15:16:13 -0600

    message encryption works

---
+++ pgp/pgp-message-encrypt.py
@@ -0,0 +1,19 @@
+import os
+import time
+import pgpy
+from pgpy import PGPMessage
+from pgpy import PGPKey
+
+
+pubkey, _ = PGPKey.from_file("C:/Users/MDA/Desktop/pgpkeys/protonmail-publickey.asc")
+msg = PGPMessage.new("asdf")
+pubkey.encrypt(msg)
+
+username = os.getlogin()
+
+datetime = time.strftime("%Y-%m-%d-%H%M%S")
+
+
+f = open(f"C:/Users/{username}/Desktop/{datetime}_ENCRYPTED_PGP_Message.txt", "w")
+f.write(str(pubkey.encrypt(msg)))
+f.close()
